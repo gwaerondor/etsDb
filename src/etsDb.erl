3c2f69870d171700f3bf05998ed4c594d59d94bb
Robin Larsson <gwaerondor@gmail.com>
2016-11-17 23:55:10 +0100

    Adds all of the advertised functionality.

--- src/etsDb.erl
+++ src/etsDb.erl
@@ -1,18 +1,48 @@
 -module(etsDb).
+-export([start/0]).
 
 start() ->
-    Tid = ets:new(database, [global, bag, {keypos, 1}]),
-    loop(Tid).
+    Tid = ets:new(database, [set, public, {keypos, 1}]),
+    spawn(fun() -> register(database, self()), 
+		   loop(Tid)
+	  end).
 
 loop(Tid) ->
     receive
 	{get, Key, Sender} ->
-	    Sender ! get_value(Tid, Key);
+	    Result = get_value(Tid, Key),
+	    Sender ! Result;
 	{delete, Key, Sender} ->
 	    Sender ! delete_value(Tid, Key);
-	{save, Key, Value, Sender} ->
-	    Sender ! save_value(Tid, Key, Value);
+	{set, Key, Value, Sender} ->
+	    Sender ! set_value(Tid, Key, Value);
 	{transform, Key, Function, Sender} ->
 	    Sender ! transform_value(Tid, Key, Function)
     end,
     loop(Tid).
+
+get_value(Tid, Key) ->
+    case ets:lookup(Tid, Key) of
+	[{_,Value}] ->
+	    Value;
+	[] ->
+	    {error, "No such key"}
+    end.
+
+delete_value(Tid, Key) ->
+    ets:delete(Tid, Key),
+    ok.
+
+set_value(Tid, Key, Value) ->
+    ets:insert(Tid, {Key, Value}),
+    ok.
+
+transform_value(Tid, Key, Function) ->
+    Value = get_value(Tid, Key),
+    case catch Function(Value) of
+	{'EXIT', _} ->
+	    {error, "Transformation function not valid for element"};
+	Transformed ->
+	    ets:insert(Tid, {Key, Transformed}),
+	    ok
+    end.
